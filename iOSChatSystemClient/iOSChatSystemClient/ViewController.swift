ebb9ab026583a00e90aeb583390baa35d9822fd3
Дмитрий Титов <dimatitov96@mail.ru>
2018-03-03 19:21:15 +0500

    Client start

---
+++ iOSChatSystemClient/iOSChatSystemClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSChatSystemClient
+//
+//  Created by Dmitrii Titov on 03.03.2018.
+//  Copyright © 2018 Dmitriy Titov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
